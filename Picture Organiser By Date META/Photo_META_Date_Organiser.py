9d8c132e6f19bbe93daacb0f945bf817055a1764
Chris Davison <c.jr.davison@gmail.com>
2017-06-03 23:32:30 +0100

    General tidying up/refactoring

--- Picture Organiser By Date META/Photo_META_Date_Organiser.py
+++ Picture Organiser By Date META/Photo_META_Date_Organiser.py
@@ -14,17 +14,18 @@ def get_exif_data(image):
     """Returns a dictionary from the exif data of an PIL Image item. Also converts the GPS Tags"""
     exif_data = {}
     info = image._getexif()
-    if info:
-        for tag, value in info.items():
-            decoded = TAGS.get(tag, tag)
-            exif_data[decoded] = value
-            if decoded == "GPSInfo":
-                gps_data = {}
-                for t in value:
-                    sub_decoded = GPSTAGS.get(t, t)
-                    gps_data[sub_decoded] = value[t]
+    if not info:
+        return exif_data
+    for tag, value in info.items():
+        decoded = TAGS.get(tag, tag)
+        exif_data[decoded] = value
+        if decoded == "GPSInfo":
+            gps_data = {}
+            for t in value:
+                sub_decoded = GPSTAGS.get(t, t)
+                gps_data[sub_decoded] = value[t]
 
-                exif_data[decoded] = gps_data
+            exif_data[decoded] = gps_data
     return exif_data
 
 
@@ -80,51 +81,59 @@ def pulling_metadata_date(image_name):
 
 
 def File_Date_Organiser(absolute_path):
-
-
+    def should_process_file(fn):
+        valid_exts = ['JPG', 'TIF', 'WAV']
+        _, ext = os.path.splitext(fn)
+        return os.path.isfile(fn) and ext.upper() in valid_exts
     os.chdir(absolute_path)
     print(os.listdir('.'), os.path.abspath(os.curdir))
     for number, file in enumerate(os.listdir()):
-        if os.path.isfile(file):
+        if should_process_file(file):
             print (number,':',file)
-            valid_exts = ['JPG', 'TIF', 'WAV']
             _, ext = os.path.splitext(file)
-            if ext.upper() in valid_exts:
-                try:
-                    date_formatted = pulling_metadata_date(file)
-                    folder_name = (str(date_formatted[:4])+'-'+str(date_formatted[5:7]))
-                    os.mkdir('{}'.format(folder_name))
-                except KeyError:
-                    print('Images Do not Contain Date Information in their METADATA')
-                    continue
-                except OSError as exc:
-                    if exc.errno != errno.EEXIST:
-                        raise exc
-                shutil.move(file, '{}'.format(folder_name))
-                print('Complete')
-
-            else:
-                print("Picture Format: .{}\nDoes not contain META info".format(file.upper().split(".")[1]))
+            try:
+                date_formatted = pulling_metadata_date(file)
+                folder_name = (str(date_formatted[:4])+'-'+str(date_formatted[5:7]))
+                os.mkdir('{}'.format(folder_name))
+            except KeyError:
+                print('Images Do not Contain Date Information in their METADATA')
+                continue
+            except OSError as exc:
+                if exc.errno != errno.EEXIST:
+                    raise exc
+            shutil.move(file, '{}'.format(folder_name))
+            print('Complete')
+        else:
+            print("Picture Format: .{}\nDoes not contain META info".format(file.upper().split(".")[1]))
 
 
 '''
 CHANGE DIRECTORY BELOW
 '''
 
+def ask_and_confirm(question):
+    ans1 = input(question)
+    ans2 = input("Please confirm my repeating response:\n")
+    if ans1 == ans2:
+        return ans1
+    return None
+
+
+def ask_until_confirmed(question):
+    address = None
+    while not address:
+        address = ask_and_confirm(question)
+    return address
 
 
 def main():
-    directory_address = input("Enter Directory with pictures you wish to organise by Date YYYY-MM:\n")
-    confirm_directory_address = input("Confirm Directory by retyping:\n")
-
-    if directory_address == confirm_directory_address:
-        if os.path.isdir(confirm_directory_address):
-            File_Date_Organiser(directory_address)
-        else:
-            print("Directory: {}\nDoes Not Exist".format(confirm_directory_address))#Put the Explicit directory of the folder you wish to organise
-            main()
+    question = "Enter Directory with pictures you wish to organise by Date YYYY-MM:\n"
+    address = ask_until_confirmed(question)
+    if os.path.isdir(confirm_directory_address):
+        File_Date_Organiser(directory_address)
     else:
-        print("confirmation Directories did not match")
+        print("Directory: {}\nDoes Not Exist".format(confirm_directory_address))#Put the Explicit directory of the folder you wish to organise
         main()
+
 if __name__ == "__main__":
     main()
